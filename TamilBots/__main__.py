b50f6dc2711ff17ced384c8f17c0f7b19998c87c
Sanobarbek <102126583+Sanobarbek@users.noreply.github.com>
2022-03-22 08:36:26 +0500

    Update __main__.py

--- TamilBots/__main__.py
+++ TamilBots/__main__.py
@@ -36,9 +36,9 @@ async def start(client, message):
     name = message.from_user["first_name"]
     if message.chat.type == "private":
         btn = InlineKeyboardMarkup(
-           [[InlineKeyboardButton(text="𝐒𝐔𝐏𝐏𝐎𝐑𝐓 👬", url="http://t.me/TamilSupport"),
+           [[InlineKeyboardButton(text="YORDAM 🛠️", url="http://t.me/TamilSupport"),
              InlineKeyboardButton(
-                        text="𝐀𝐃𝐃 𝐌𝐄 🤗", url="http://t.me/SongProBot?startgroup=true"
+                        text="GURUHGA  🤗", url="http://t.me/SongProBot?startgroup=true"
                     )
                 ]
             ]
